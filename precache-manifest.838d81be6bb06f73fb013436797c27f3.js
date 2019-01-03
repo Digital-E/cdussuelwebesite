fbfb1b93ef6495760d943cd6cdbab17189b1c8d8
Samuel Bassett <samabassett@gmail.com>
2019-01-03 11:44:12 +0100

    Updates

---
+++ precache-manifest.838d81be6bb06f73fb013436797c27f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "617ce412e010b9d0691c679f25f7e0cd",
+    "url": "/static/media/info.617ce412.svg"
+  },
+  {
+    "revision": "a8667e92e5f0b1bd3e98",
+    "url": "/static/css/main.51da573c.chunk.css"
+  },
+  {
+    "revision": "21ba0040bae49f616016f93d817574c9",
+    "url": "/static/media/Lack-Regular.21ba0040.woff"
+  },
+  {
+    "revision": "c453b62cdd4df4987b13",
+    "url": "/static/js/1.c453b62c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8667e92e5f0b1bd3e98",
+    "url": "/static/js/main.a8667e92.chunk.js"
+  },
+  {
+    "revision": "557f82420caf828ac6dd15573c79ee06",
+    "url": "/static/media/Lack-Regular.557f8242.eot"
+  },
+  {
+    "revision": "ea10844cf16578aa08b5d71886247b36",
+    "url": "/static/media/Lack-Regular.ea10844c.ttf"
+  },
+  {
+    "revision": "2ba06ebe0f91838cd994b452264f3d54",
+    "url": "/static/media/Lack-Regular.2ba06ebe.woff2"
+  },
+  {
+    "revision": "c453b62cdd4df4987b13",
+    "url": "/static/css/1.10850593.chunk.css"
+  },
+  {
+    "revision": "3646a957c0e740a75920951d5feef4bf",
+    "url": "/index.html"
+  }
+];
